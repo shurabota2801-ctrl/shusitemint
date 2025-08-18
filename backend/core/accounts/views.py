88eed003a669b96934294d4a7751b3367405679c
shurabota <shurabota2801@gmail.com>
2025-08-18 12:55:47 +0500

    first commit

--- backend/core/accounts/views.py
+++ backend/core/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
 
-# Create your views here.
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
