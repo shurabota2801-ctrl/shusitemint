3033b65495587500773c164e283312c67aa0d998
shurabota <shurabota2801@gmail.com>
2025-08-18 21:54:46 +0500

    first commit

--- backend/core/accounts/views.py
+++ backend/core/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token 
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
@@ -10,3 +11,14 @@ class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
 
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username
+        })
